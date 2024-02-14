6755f6fb8fa6a5a3783dd88106ff0887a8972cf5
Charlotta Grundmann <lotta@grundmann.se>
2024-02-14 14:23:12 +0000

    Add add tasks functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+// date picker initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+// select option initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
